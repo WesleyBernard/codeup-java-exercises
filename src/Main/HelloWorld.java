c8928ccef6dacd37e5cefd7dca624f9c011683c4
Wesley Bernard <wesley.bernard64@gmail.com>
2022-02-28 15:57:33 -0500

    Completing variables exercise

--- src/Main/HelloWorld.java
+++ src/Main/HelloWorld.java
@@ -2,7 +2,48 @@ package Main;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        System.out.print("\n");
+//        System.out.print("Hello World!");
+//        System.out.print("\n");
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "omegalol";
+        System.out.println(myString);
+//        char myChar = 'B';
+//        myString = myChar
+//        char myString = "c";
+//        myString = String.valueOf(3.14159);
+//        System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+//        System.out.println(three);
+         x = 4;
+//        x = x + 5;
+        x++;
+
+         x = 3;
+        int y = 4;
+        y *= x;
+        System.out.println(y);
+
+         x = 10;
+         y = 2;
+         x /= y;
+        y -= x;
+
+
+
     }
 }

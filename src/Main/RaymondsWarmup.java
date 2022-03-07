2b3ee10fa10db237b65be6684e30c1e2d4354449
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-07 16:26:09 -0500

    Learning about decimal format and set rounding mode

--- src/Main/RaymondsWarmup.java
+++ src/Main/RaymondsWarmup.java
@@ -1,20 +1,26 @@
 package Main;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class RaymondsWarmup {
     public static void main(String[] args) {
-        shouldWakeUp (true, 1);
+//        shouldWakeUp (true, 1);
+//
+//            shouldWakeUp (false, 2);
+//
+//            shouldWakeUp (true, 8);
+//
+//            shouldWakeUp (true, -1);
+//        shouldWakeUp(true, 1);
+//        shouldWakeUp(false, 2);
+//        shouldWakeUp(true, 8);
+//        shouldWakeUp(true, -1);
+//        shouldWakeUp(true, 0);
+//        shouldWakeUp(true, 44);
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.175, 3.176));
 
-            shouldWakeUp (false, 2);
-
-            shouldWakeUp (true, 8);
-
-            shouldWakeUp (true, -1);
-        shouldWakeUp(true, 1);
-        shouldWakeUp(false, 2);
-        shouldWakeUp(true, 8);
-        shouldWakeUp(true, -1);
-        shouldWakeUp(true, 0);
-        shouldWakeUp(true, 44);
 
     }
 //    We have a dog that likes to bark.  We need to wake up if the dog is barking at night!
@@ -46,15 +52,47 @@ public class RaymondsWarmup {
 //    NOTE: The shouldWakeUp method  needs to be defined as public static like we have been doing so far in the course.
 //
 //    NOTE: Do not add a  main method to solution code.
-    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
-        if (hourOfDay < 0 || hourOfDay > 23) {
-            System.out.println("false");
-          return false;
-        }else if (barking == true && (hourOfDay < 8 || hourOfDay > 22)){
-            System.out.println("true");
-            return true;
-        }
-        System.out.println("false");
-        return false;
-    }
+//    SOLUTION
+//    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+//        if (hourOfDay < 0 || hourOfDay > 23) {
+//            System.out.println("false");
+//          return false;
+//        }else if (barking == true && (hourOfDay < 8 || hourOfDay > 22)){
+//            System.out.println("true");
+//            return true;
+//        }
+//        System.out.println("false");
+//        return false;
+//    }
+//    SOLUTION
+//Write a method areEqualByThreeDecimalPlaces with two parameters of type double.
+//
+//    The method should return boolean and it needs to return true if two double numbers are the same up to three decimal places. Otherwise, return false.
+//
+//
+//    EXAMPLES OF INPUT/OUTPUT:
+//
+//            * areEqualByThreeDecimalPlaces(-3.1756, -3.175); → should return true since numbers are equal up to 3 decimal places.
+//
+//            * areEqualByThreeDecimalPlaces(3.175, 3.176); → should return false since numbers are not equal up to 3 decimal places
+//
+//* areEqualByThreeDecimalPlaces(3.0, 3.0); → should return true since numbers are equal up to 3 decimal places.
+//
+//            * areEqualByThreeDecimalPlaces(-3.123, 3.123); → should return false since numbers are not equal up to 3 decimal places.
+//
+//
+//    TIP: Use paper and pencil.
+//
+//    TIP: Use casting.
+//
+//    NOTE: The areEqualByThreeDecimalPlaces method  needs to be defined as public static like we have been doing so far in the course.
+//    NOTE: Do not add a  main method to solution code.
+//    SOLUTION
+public static boolean areEqualByThreeDecimalPlaces (double firstNumber, double secondNumber) {
+    DecimalFormat df = new DecimalFormat("#.###");
+    df.setRoundingMode(RoundingMode.DOWN);
+
+    return df.format(firstNumber).equals(df.format(secondNumber));
+}
+
 }

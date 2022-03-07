2e39164c075f543b5ccfa7901830a660db9e3420
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-07 11:49:24 -0500

    reformatting circles exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -5,9 +5,8 @@ import Util.Input;
 public class CircleApp {
     public static void main(String[] args) {
         Input uwu = new Input();
-        double radious = uwu.getDouble();
         Circle myPie = new Circle();
-        myPie.setRadious(radious);
+        myPie.setRadious(uwu.getDouble());
         myPie.getArea();
         myPie.getCircumference();
     }

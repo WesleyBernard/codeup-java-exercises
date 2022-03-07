952ff59c8d32805c97222cc5076dca87d862c974
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-07 10:32:21 -0500

    Completing Person exercise

--- src/Main/Person.java
+++ src/Main/Person.java
@@ -2,10 +2,30 @@ package Main;
 
 public class Person {
     public static void main(String[] args) {
-        Person Wesley = new Person();
-        Wesley.setName("Wesley");
-        System.out.println(Wesley.getName());
-        Wesley.sayHello();
+//        Person Wesley = new Person();
+//        Wesley.setName("Wesley");
+//        System.out.println(Wesley.getName());
+//        Wesley.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+    }
+    public Person(String name) {
+        this.name = name;
+    }
+    public Person() {
+        this.name = "NoName";
     }
     private String name;
 

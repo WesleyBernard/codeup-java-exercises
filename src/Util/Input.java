e69d2627acbaddb4f7781a57d20a4df417ac51ca
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-07 11:20:29 -0500

    Completing util scanner exercise

--- src/Util/Input.java
+++ src/Util/Input.java
@@ -3,11 +3,7 @@ package Util;
 import java.util.Scanner;
 
 public class Input {
-    public static void main(String[] args) {
-        Input uwu = new Input();
-        uwu.getString();
-        uwu.getBoolean();
-    }
+
     private Scanner scanner;
     private String name;
     public Input(){
@@ -38,5 +34,37 @@ public class Input {
         }
         System.out.println("Couldn't be more false");
         return false;
+    }
+    public int getInt(int min, int max){
+        int UserInput;
+        do {
+            System.out.printf("please input a INTEGER between %d and %d ",min, max);
+            UserInput = this.scanner.nextInt();
+        } while (UserInput > max || UserInput < min);
+        System.out.println("thanks UwU");
+        this.scanner.nextLine();
+        return UserInput;
+    }
+    public int getInt(){
+        System.out.print("Please input an INTEGER. UwU ");
+        int userInput = this.scanner.nextInt();
+        System.out.println("I'll keep that number in my pocket for ya!");
+        return userInput;
+    }
+    public double getDouble(double min, double max){
+        double UserInput;
+        do {
+            System.out.printf("please input a DOUBLE between %f and %f ",min, max);
+            UserInput = this.scanner.nextDouble();
+        } while (UserInput > max || UserInput < min);
+        System.out.println("thanks UwU");
+        this.scanner.nextLine();
+        return UserInput;
+    }
+    public double getDouble(){
+        System.out.print("Please enter a DOUBLE. UwU ");
+        double userInput = this.scanner.nextDouble();
+        System.out.println("I'll hold onto that for ya");
+        return userInput;
     }
 }
---
+++ src/Util/InputTest.java
@@ -0,0 +1,11 @@
+package Util;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Input uwu = new Input();
+        uwu.getString();
+        uwu.getBoolean();
+        uwu.getInt(10, 100);
+        uwu.getDouble();
+    }
+}

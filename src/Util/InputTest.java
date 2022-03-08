4542e17818d956888b9b611f887d2f1672c84c75
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-08 13:35:10 -0500

    Updating input util test

--- src/Util/InputTest.java
+++ src/Util/InputTest.java
@@ -3,10 +3,10 @@ package Util;
 public class InputTest {
     public static void main(String[] args) {
         Input uwu = new Input();
-        uwu.getString();
-        uwu.getBoolean();
-        uwu.getInt(10, 100);
-        uwu.getDouble();
+        uwu.getString("pls enter a string");
+        uwu.getBoolean("Enter [y/n]");
+        uwu.getInt(10, 100, "Enter a number between 10 and 100");
+        uwu.getDouble("Please enter a double");
         uwu.close();
         uwu.getDouble();
     }

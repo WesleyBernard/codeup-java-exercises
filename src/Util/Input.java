1b800d264da9fb4a5aa7d05756b8f296e1d73cf2
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-08 12:01:40 -0500

    Reformatting input util

--- src/Util/Input.java
+++ src/Util/Input.java
@@ -20,13 +20,23 @@ public class Input {
         System.out.println(this.name);
     }
     public String getString(){
-        System.out.print("Please input a string! UwU ");
-        String theString = this.scanner.nextLine();
-        System.out.println(theString);
-        return theString;
+        return this.scanner.nextLine();
+    }
+    public String getString(String prompt){
+        System.out.println(prompt);
+        return this.scanner.nextLine();
     }
     public Boolean getBoolean(){
-        System.out.print("Please enter \"y\" or \"yes\" to confirm! All other inputs will be read as \"false\" or \"no\" UwU ");
+        String UserInput = this.scanner.next();
+        if(UserInput.equalsIgnoreCase("y")||UserInput.equalsIgnoreCase("yes")){
+            System.out.println("So true, besty");
+            return true;
+        }
+        System.out.println("Couldn't be more false");
+        return false;
+    }
+    public Boolean getBoolean(String prompt){
+        System.out.printf("%s %n", prompt);
         String UserInput = this.scanner.next();
         if(UserInput.equalsIgnoreCase("y")||UserInput.equalsIgnoreCase("yes")){
             System.out.println("So true, besty");
@@ -38,34 +48,50 @@ public class Input {
     public int getInt(int min, int max){
         int UserInput;
         do {
-            System.out.printf("please input a INTEGER between %d and %d ",min, max);
             UserInput = this.scanner.nextInt();
         } while (UserInput > max || UserInput < min);
-        System.out.println("thanks UwU");
+        this.scanner.nextLine();
+        return UserInput;
+    }
+    public int getInt(int min, int max, String prompt){
+        int UserInput;
+        do {
+            System.out.printf("%s ",prompt);
+            UserInput = this.scanner.nextInt();
+        } while (UserInput > max || UserInput < min);
         this.scanner.nextLine();
         return UserInput;
     }
     public int getInt(){
-        System.out.print("Please input an INTEGER. UwU ");
-        int userInput = this.scanner.nextInt();
-        System.out.println("I'll keep that number in my pocket for ya!");
-        return userInput;
+        return this.scanner.nextInt();
+    }
+    public int getInt(String prompt){
+        System.out.printf("%s ", prompt);
+        return this.scanner.nextInt();
     }
     public double getDouble(double min, double max){
         double UserInput;
         do {
-            System.out.printf("please input a DOUBLE between %f and %f ",min, max);
             UserInput = this.scanner.nextDouble();
         } while (UserInput > max || UserInput < min);
-        System.out.println("thanks UwU");
+        this.scanner.nextLine();
+        return UserInput;
+    }
+    public double getDouble(double min, double max, String prompt){
+        double UserInput;
+        do {
+            System.out.print(prompt + " ");
+            UserInput = this.scanner.nextDouble();
+        } while (UserInput > max || UserInput < min);
         this.scanner.nextLine();
         return UserInput;
     }
     public double getDouble(){
-        System.out.print("Please enter a DOUBLE. UwU ");
-        double userInput = this.scanner.nextDouble();
-        System.out.println("I'll hold onto that for ya");
-        return userInput;
+        return this.scanner.nextDouble();
+    }
+    public double getDouble(String prompt){
+        System.out.print(prompt + " ");
+        return this.scanner.nextDouble();
     }
     public void close(){
         this.scanner.close();

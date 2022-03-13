a9c397bae54c74145db0e2fbaa7fef784a01fee1
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-13 13:44:55 -0400

    updating grocery list

--- src/groceryList/GroceryListApp.java
+++ src/groceryList/GroceryListApp.java
@@ -7,13 +7,16 @@ public class GroceryListApp {
     public static void main(String[] args) {
         ArrayList<HashMap> groceryList = new ArrayList<>();
         HashMap<String, Integer> produce = new HashMap<>();
+        HashMap<String ,Integer> dairy = new HashMap<>();
+        HashMap<String ,Integer> seafood = new HashMap<>();
+        HashMap<String ,Integer> pantry = new HashMap<>();
         groceryList.add(produce);
-        groceryList.get(groceryList.indexOf(produce)).put("onions", 8);
+        groceryList.add(dairy);
+//        groceryList.get(groceryList.indexOf(produce)).put("onions", 8);
         System.out.println(groceryList);
         produce.put("carrots", 4);
         System.out.println(groceryList);
-        HashMap<String, Integer> dairy = new HashMap<>();
-        groceryList.add(dairy);
+
         dairy.put("greek yogurt", 5);
         System.out.println(groceryList);
         System.out.println("Displaying dairy category....");

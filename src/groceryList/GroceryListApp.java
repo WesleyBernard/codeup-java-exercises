eb386f9a98f8f57dcc33efbd69d1205e9afc1af6
Wesley Bernard <wesley.bernard64@gmail.com>
2022-03-14 04:43:23 -0400

    Completing grocery list

--- src/groceryList/GroceryListApp.java
+++ src/groceryList/GroceryListApp.java
@@ -1,20 +1,25 @@
 package groceryList;
 
 import Util.Input;
+import com.sun.source.tree.Tree;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class GroceryListApp {
     public static void main(String[] args) {
         Input uwu = new Input("OwO");
-        ArrayList<HashMap> groceryList = new ArrayList<>();
+        ArrayList<Map> groceryList = new ArrayList<>();
         HashMap<String, Integer> produce = new HashMap<>();
         HashMap<String ,Integer> dairy = new HashMap<>();
         HashMap<String ,Integer> seafood = new HashMap<>();
         HashMap<String ,Integer> pantry = new HashMap<>();
-        groceryList.add(produce);
-        groceryList.add(dairy);
+//        groceryList.add(produce);
+//        groceryList.add(dairy);
+//        groceryList.add(seafood);
+//        groceryList.add(pantry);
 //        groceryList.get(groceryList.indexOf(produce)).put("onions", 8);
 //        System.out.println(groceryList);
 //        produce.put("carrots", 4);
@@ -25,29 +30,47 @@ public class GroceryListApp {
 //        System.out.println("Displaying dairy category....");
 //        System.out.println(groceryList);
         Boolean finalizeList = false;
-        do {
-            String userInput = uwu.getString("Welcome to your grocery list, would you like to add an item? [y/n]");
-            if (userInput.equalsIgnoreCase("y")){
-                System.out.println("Please select a category");
-                userInput = uwu.getString("[produce] [dairy] [seafood] [pantry]");
-            }
-            if (userInput.equalsIgnoreCase("produce")){
-                produce.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
-            }
-            if (userInput.equalsIgnoreCase("dairy")){
-                dairy.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
-            }
-            if (userInput.equalsIgnoreCase("pantry")){
-                pantry.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
-            }
-            if (userInput.equalsIgnoreCase("seafood")){
-                seafood.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
-            }
-            else{
-                System.out.println(groceryList);
-            }
-            finalizeList = uwu.getBoolean("Finalize list? [y/n]");
-        } while (!finalizeList);
+        if(uwu.getBoolean("Would you like to make a grocery list? ")) {
+            do {
+                String userInput = uwu.getString("Would you like to add an item? [y/n]");
+                if (userInput.equalsIgnoreCase("y")) {
+                    System.out.println("Please select a category");
+                    userInput = uwu.getString("[produce] [dairy] [seafood] [pantry]");
+                }
+                if (userInput.equalsIgnoreCase("produce")) {
+                    produce.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
+                }
+                if (userInput.equalsIgnoreCase("dairy")) {
+                    dairy.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
+                }
+                if (userInput.equalsIgnoreCase("pantry")) {
+                    pantry.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
+                }
+                if (userInput.equalsIgnoreCase("seafood")) {
+                    seafood.put(uwu.getString("please enter Item"), uwu.getInt("please enter quantity"));
+                }
+                finalizeList = uwu.getBoolean("Finalize list? [y/n]");
+                if (finalizeList) {
+                    Map<String, Integer> sortedProduce = new TreeMap<>(produce);
+                    Map<String, Integer> sortedseafood = new TreeMap<>(seafood);
+                    Map<String, Integer> sortedPantry = new TreeMap<>(pantry);
+                    Map<String, Integer> sortedDairy = new TreeMap<>(dairy);
+                    if(!sortedProduce.isEmpty()) {
+                        groceryList.add(sortedProduce);
+                    }
+                    if(!sortedDairy.isEmpty()) {
+                        groceryList.add(sortedDairy);
+                    }
+                    if(!sortedseafood.isEmpty()) {
+                        groceryList.add(sortedseafood);
+                    }
+                    if(!sortedPantry.isEmpty()) {
+                        groceryList.add(sortedPantry);
+                    }
+                    System.out.println(groceryList);
+                }
+            } while (!finalizeList);
+        }
     }
 //    public static void addItem(HashMap<String, Integer> category){
 //        category.put()
